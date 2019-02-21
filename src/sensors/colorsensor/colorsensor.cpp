71f0a10eefe926f9efdc7bb9fc938fd34d495190
Brennan Koopman <brennankoopman@gmail.com>
2019-02-21 12:58:50 -0500

    Color sensor v2 (#3)
    * further polishing of the color sensor + ex file
    
    * changes and example code for col sen
    
    * fixing constants
    
    * addressing comments.

--- src/sensors/colorsensor/colorsensor.cpp
+++ src/sensors/colorsensor/colorsensor.cpp
@@ -1,25 +1,19 @@
-//This is the class wrapper for the COLOR_SENSOR color sensor
+//This is the class wrapper for the color sensor
 
-#include "color_sensor.h"
+#include "colorsensor.h"
+#include <Arduino.h>
 
-//CONSTRUCTORS_____________________________________________________________________________________________
-COLOR_SENSOR::COLOR_SENSOR()
-{
-   _freq_gain = FREQ_GAIN_LOW;    //LOWEST SETTING BY DEFAULT
-   _delay_time = 100;             //in ms
-}
-
-//constructor with configurations outside of default
+//CONSTRUCTOR_____________________________________________________________________________________________
 //@param freq_gain - 1 MAX freq (500->600kHz), 2 - MED freq (100->120kHz), 3 - LOW freq (10->12kHz)
-COLOR_SENSOR::COLOR_SENSOR(freqGain freq_gain_, uint16_t delay_time_)
+ColorSensor::ColorSensor(freqGainLevel freq_gain = FREQ_GAIN_LOW, uint16_t delay_time = 100)
 {
-    _freq_gain = freq_gain;
-    _delay_time = delay_time;
+    freq_gain_ = freq_gain;
+    delay_time_ = delay_time;
 }
 
 //PUBLIC_________________________________________________________________________________________________________
 
-void COLOR_SENSOR::initialize()
+void ColorSensor::initialize()
 {
      // Setting the outputs
     pinMode(COLOR_SENSOR_PIN0, OUTPUT);
@@ -31,72 +25,75 @@ void COLOR_SENSOR::initialize()
     pinMode(COLOR_SENSOR_OUT, INPUT);
     
     // Setting frequency scaling to 20% by default
-    digitalWrite(COLOR_SENSOR_PIN0,LOW);
-    digitalWrite(COLOR_SENSOR_PIN1,HIGH);
+    digitalWrite(COLOR_SENSOR_PIN0, LOW);
+    digitalWrite(COLOR_SENSOR_PIN1, HIGH);
 
      // Set the frequency scaling for the sensor
-    if(freq_gain == 1)
+    if(freq_gain_ == 1)
     {
-        digitalWrite(COLOR_SENSOR_PIN0,HIGH);
-        digitalWrite(COLOR_SENSOR_PIN1,HIGH);
+        digitalWrite(COLOR_SENSOR_PIN0, HIGH);
+        digitalWrite(COLOR_SENSOR_PIN1, HIGH);
     }
-    else if(freq_gain == 2)
+    else if(freq_gain_ == 2)
     {
-        digitalWrite(COLOR_SENSOR_PIN0,HIGH);
-        digitalWrite(COLOR_SENSOR_PIN1,LOW);
+        digitalWrite(COLOR_SENSOR_PIN0, HIGH);
+        digitalWrite(COLOR_SENSOR_PIN1, LOW);
     }
-    else if(freq_gain == 3)
+    else if(freq_gain_ == 3)
     {
-        digitalWrite(COLOR_SENSOR_PIN0,LOW);
-        digitalWrite(COLOR_SENSOR_PIN1,HIGH);
+        digitalWrite(COLOR_SENSOR_PIN0, LOW);
+        digitalWrite(COLOR_SENSOR_PIN1, HIGH);
     }
 }
 
 // returns the raw sensor data for the red filter
-uint16_t COLOR_SENSOR::read_red()
+uint16_t ColorSensor::read_red()
 {
 // Setting RED (R) filtered photodiodes to be read
-  digitalWrite(COLOR_SENSOR_PIN2,LOW);
-  digitalWrite(COLOR_SENSOR_PIN3,LOW);
+  digitalWrite(COLOR_SENSOR_PIN2, LOW);
+  digitalWrite(COLOR_SENSOR_PIN3, LOW);
   // Delay to prevent the sensor from being read too often
-  delay(_delay_time);
+  delay(delay_time_);
   // Reading the output frequency
   // unsure how accurate this function is....
   return pulseIn(COLOR_SENSOR_OUT, LOW);
 }
 
 // returns the raw sensor data for the green filter
-uint16_t COLOR_SENSOR::read_green()
+uint16_t ColorSensor::read_green()
 {
 // Setting GREEN (G) filtered photodiodes to be read
-  digitalWrite(COLOR_SENSOR_PIN2,HIGH);
-  digitalWrite(COLOR_SENSOR_PIN3,HIGH);
+  digitalWrite(COLOR_SENSOR_PIN2, HIGH);
+  digitalWrite(COLOR_SENSOR_PIN3, HIGH);
   // Delay to prevent the sensor from being read too often
-  delay(_delay_time);
+  delay(delay_time_);
   // Reading the output frequency
   return pulseIn(COLOR_SENSOR_OUT, LOW);
 }
 
 // returns the raw sensor data for the blue filter
-uint16_t COLOR_SENSOR::read_blue()
+uint16_t ColorSensor::read_blue()
 {
 // Setting BLUE (B) filtered photodiodes to be read
-  digitalWrite(COLOR_SENSOR_PIN2,LOW);
-  digitalWrite(COLOR_SENSOR_PIN3,HIGH);
+  digitalWrite(COLOR_SENSOR_PIN2, LOW);
+  digitalWrite(COLOR_SENSOR_PIN3, HIGH);
   // Delay to prevent the sensor from being read too often
-  delay(_delay_time);
+  delay(delay_time_);
   // Reading the output frequency
   return pulseIn(COLOR_SENSOR_OUT, LOW);
 
 }
 
 // returns the current terrain based on the sensor data, and the experimental averages and STDEVs
-uint8_t COLOR_SENSOR::curr_terrain()
+// 0 -> grav, 1 -> water, 2 -> wood, 3-> sand
+uint8_t ColorSensor::curr_terrain(bool debug = false)
 {
     uint16_t r = read_red();
     uint16_t b = read_blue();
     uint16_t g = read_green(); 
     uint8_t min_index = 0;
+
+    int32_t error[4] = {};
     
     for(int i = 0; i < 4; i++)
     {
@@ -104,10 +101,25 @@ uint8_t COLOR_SENSOR::curr_terrain()
         error[i] = (r - AVG_R[i])*(r - AVG_R[i]) + (g - AVG_G[i])*(g - AVG_G[i]) + (b - AVG_B[i])*(b - AVG_B[i]);
         if(error[min_index] >= error[i])
         {
-        min_index = i;
+            min_index = i;
         }
     }
-
+    if(debug)
+    {
+        Serial.print(r);
+        Serial.print(",");
+        Serial.print(g);
+        Serial.print(",");
+        Serial.println(b);
+        
+        // the print order is gravel, water, wood, sand
+        for( int j = 0; j<4; j++)
+        {
+            Serial.print(error[j]);
+            Serial.print(",");
+        }
+        Serial.println();
+    }
     if(error[min_index] < dev[min_index])
     {
         return min_index;
