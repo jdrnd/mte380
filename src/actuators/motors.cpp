63d536c4af1474daeb29936a434d8626c018e681
Joel Ruhland <jdrnd@users.noreply.github.com>
2019-02-28 15:20:16 -0500

    initial motors integration (#5)
    Speed and direction control with speed feedback

---
+++ src/actuators/motors.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <math.h>
+
+#include "motors.h"
+
+
+Motor* Motors::left = new Motor;
+Motor* Motors::right = new Motor;
+
+void Motors::leftMotorInterrupt() {
+    left->update();
+}
+
+void Motors::rightMotorInterrupt() {
+    right->update();
+}
+
+// Initializes motor with pins and sets up interrupts
+// Must be ran before motor.setSpeed() can be used
+void Motor::init(uint8_t pwm_pin, uint8_t enable_pin, uint8_t sensor1_pin, uint8_t sensor2_pin) {
+    pwm_pin_ = pwm_pin;
+    enable_pin_ = enable_pin;
+    sensor1_pin_ = sensor1_pin;
+    sensor2_pin_ = sensor2_pin;
+
+    pinMode(sensor2_pin, INPUT);
+    if (sensor1_pin == LEFT_MOTOR_SENSOR_A_PIN) {
+        attachInterrupt(digitalPinToInterrupt(sensor1_pin), Motors::leftMotorInterrupt, RISING);
+    }
+    else if (sensor1_pin == RIGHT_MOTOR_SENSOR_A_PIN) {
+        attachInterrupt(digitalPinToInterrupt(sensor1_pin), Motors::rightMotorInterrupt, RISING);
+    }
+    pinMode(enable_pin_, OUTPUT);
+
+    last_ = micros();
+}
+
+// Accepts value from -100 to +100 corresponding to speed
+// Should not be used with a value of 0, stop() should be called instead
+void Motor::setSpeed(int8_t speedval) {
+    speedval = map(speedval, -100,100, 0, 255);
+    Serial.print(speedval);
+    digitalWrite(enable_pin_, HIGH);
+    analogWrite(pwm_pin_, speedval);
+
+}
+
+// Stops motor immediatly 
+void Motor::stop() {
+    digitalWrite(enable_pin_, LOW);
+}
+
+// Interrupt service routine called when a motor encoder encounters an edge
+// Sets speed and direction in the motor class
+void Motor::update() {
+
+    if (digitalRead(LEFT_MOTOR_SENSOR_B_PIN) == HIGH) {
+        //Serial.println("Forwards");
+        direction = 1;
+    }
+    else {
+        //Serial.println("Backwards");
+        direction = -1;
+    }
+
+    unsigned long now = micros();
+    unsigned long delta = now - last_;
+
+    // 90 measurements, so 4 degrees per measurement however we only interupt on one rising edge (8 degrees/measurement)
+    speed = 8/(delta*1.0*10e-6);
+    Serial.println((int)speed); // currently in degrees/s
+    last_ = now;
+}
+
+
+
+
---
+++ src/actuators/motors.h
@@ -0,0 +1,61 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+
+#define NUM_COUNTS_PER_REVOLUTION 90
+
+#define LEFT_MOTOR_SENSOR_A_PIN 2
+#define LEFT_MOTOR_SENSOR_B_PIN 10
+
+#define LEFT_MOTOR_ENABLE_PIN 12
+#define LEFT_MOTOR_PWM_PIN 5
+
+// TODO set these
+#define RIGHT_MOTOR_SENSOR_A_PIN 0
+#define RIGHT_MOTOR_SENSOR_B_PIN 0
+
+/*
+motor->init({PINS}) must be called first for each motor before setSpeed is used
+
+Wiring:
+- Each motor should be wired to the power supply
+- Each encoder has 2 hall effect lines, a hall effect ground/5V that need to be wired to the arduino
+- The motors PWM and enable (labeled DIR on board) should be wired to arduino. *PWM in code goes to DIR on motor board and enable in code goes to PWM on board*
+    - PWM and EN ground should go to arduino ground as well
+*/
+
+// Forward declaration so Motors knows that Motor is a class
+class Motor;
+
+// Use static class members so we can access them from interupts
+class Motors {
+    public:
+
+        static Motor *left;
+        static Motor *right;
+
+        static void leftMotorInterrupt();
+        static void rightMotorInterrupt();
+};
+
+class Motor {
+    public:
+        void init(uint8_t, uint8_t, uint8_t, uint8_t);
+
+        
+        uint8_t direction;
+        double speed;
+
+        void setSpeed(int8_t);
+        void stop();
+
+        // ISR when encoder encounters an edge
+        void update();
+    private:
+        unsigned long last_;
+        uint8_t pwm_pin_;
+        uint8_t enable_pin_;
+        uint8_t sensor1_pin_;
+        uint8_t sensor2_pin_;
+};
+
+#endif
